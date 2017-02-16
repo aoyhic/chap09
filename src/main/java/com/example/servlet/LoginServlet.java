9aca2ac5f8ecd942604971dd321f2d6b0d03a69a
java <java>
2017-02-16 14:06:51 +0900

    chap09 new

---
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -0,0 +1,59 @@
+package com.example.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+	static Log Log = LogFactory.getLog(LoginServlet.class);
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Log.info("#######");
+		Log.info("doGet()");
+		Log.info("#######");
+		
+		
+		String error = request.getParameter("error");
+		Log.info("error=["+error+"]");
+		if(error != null)
+			request.setAttribute("errorMessage", "ID, PW를 확인하세요");
+		
+			
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/loginform.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Log.info("########");
+		Log.info("doPost()");
+		Log.info("########");
+		
+		String id=request.getParameter("id");
+		String pw=request.getParameter("pw");
+		Log.info("id="+id); Log.info("pw"+pw);
+		
+		/*
+		 *  인증처리(이건 db를 배워야 할 수 있는 것. 일단 가상으로 만들어보자)
+		 */
+		
+		if ("hong".equals(id) && "1234".equals(pw)){
+			//인증성공. id는 hong, pw=1234 
+			response.sendRedirect("/index.jsp");
+		} else {
+			//인증실패 
+			response.sendRedirect("/login?error=true");	
+			
+		}
+		 
+	}
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/loginform.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>loginform.jsp</title>
+<!-- 1.animate css -->
+<link rel="stylesheet" href="/webjars/animate.css/3.5.2/animate.min.css">
+<!-- 2.BootStrap -->
+<link rel="stylesheet" href="/webjars/bootstrap/3.3.7-1/css/bootstrap.css">
+<!-- 3.JQuery -->
+<script type="text/javascript" src="/webjars/jquery/1.11.1/jquery.min.js"></script>
+<!-- 4.bootstrap.js -->
+<script type="text/javascript" src="/webjars/bootstrap/3.3.7-1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Login</h1>
+<%
+	if (request.getParameter("error") != null)
+		out.println("<h2>"+ request.getAttribute("errorMessage")+ "</h2>");
+%>
+<form action="/login" method="post">
+	id=<input name="id"><br>
+	pw=<input name="pw"><br>
+	<input type="submit">
+
+</form>
+</body>
+</html>
